0da5ddf234a2f1ffec75e4cb7d60915e82b61f37
LonNV <nguyenvanlon444@gmail.com>
2024-05-27 23:56:04 +0900

    Add dark mode

---
+++ src/app/Providers.jsx
@@ -0,0 +1,10 @@
+"user client";
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }) {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="">{children}</div>
+    </ThemeProvider>
+  );
+}
