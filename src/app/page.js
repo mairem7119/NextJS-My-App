185d53aa28290661f54537c9c39557dbeb12df9d
LonNV <nguyenvanlon444@gmail.com>
2024-05-28 18:04:22 +0900

    Fetch movie API 1.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
-import React from 'react'
+const API_KEY = process.env.API_KEY;
+import Result from "@/components/Result";
+import React from "react";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const results = data.results;
+
   return (
     <div>
-      home
+      <Result results={results} />
     </div>
-  )
+  );
 }
