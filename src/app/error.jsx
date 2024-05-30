ddffc2304713d3c437fa63a0212f45e0bedde979
LonNV <nguyenvanlon444@gmail.com>
2024-05-30 09:46:50 +0900

    Change Error function name

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -2,10 +2,11 @@
 import React from "react";
 import { useEffect } from "react";
 
-export default function error({ error, reset }) {
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
+
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong. Please try again later</h1>
